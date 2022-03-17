aaf2089bf0002cde31721dbb135bc0df8b7f8742
chocolatechen <B1029027@cgu.edu.tw>
2022-03-17 17:00:01 +0800

    add readme

---
+++ lab2/main.c
@@ -0,0 +1,107 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+
+typedef struct Lotto_record {
+	int lotto_no;
+	int lotto_receipt;
+	int emp_id;
+	char lotto_date;
+	int lotto_time;
+} typeLotto_record_t;
+
+int main(){
+    FILE* fp_bin;
+    FILE* fp_txt;
+    FILE* op_bin;
+    FILE* re_bin;
+    FILE* fp1_bin;
+    char szbuff[32];
+    time_t now = time(0);
+    strftime(szbuff, 100, "%Y%m%d-%H:%M:%S", localtime(&now));
+    printf("%s\n", szbuff);
+    typeLotto_record_t Lotto_record_t;
+	Lotto_record_t.lotto_receipt = Lotto_record_t.lotto_receipt *50 *1.1;
+    int count[]={1};
+    op_bin = fopen("operator_id.bin","wb+");
+    if ((fp_bin=fopen("count.bin","rb")) != NULL) {
+        fread(count, sizeof(count), 1 ,fp_bin);
+        fclose(fp_bin);
+    } else {
+        fp_bin=fopen("count.bin","wb+");
+        fwrite(count, sizeof(count), 1, fp_bin);
+        fclose(fp_bin);
+    }
+    fclose(op_bin);
+    re_bin = fopen("operator_id.bin","ab");
+    if ((fp1_bin = fopen("records.bin","rb")) != NULL) {
+        fread(count, sizeof(count), 1 ,fp1_bin);
+        fclose(fp1_bin);
+    } else {
+        fp_bin = fopen("records.bin","wb+");
+        fwrite(count, sizeof(count), 1, fp1_bin);
+        fclose(fp1_bin);
+    }
+    fclose(op_bin);
+    fclose(re_bin);
+    time_t curtime;
+    time(&curtime);
+    int arr_num[5][7],group,flag,temp,ID;
+    srand((unsigned) time(NULL));
+    printf("請輸入操作人員ID:(0~5):");
+    scanf("%d",&ID);
+    printf("歡迎光臨長庚樂透彩購機台，請問您要買幾組樂透彩:");
+    scanf("%d",&group);
+
+    char filename[50];
+    snprintf(filename, sizeof(filename), "lotto[%04d].txt",count[0]);
+    fp_txt = fopen(filename,"w+");
+    fprintf(fp_txt,"========= lotto649 =========\n");
+    fprintf(fp_txt,"========+ No.%05d +========\n",count[0]);
+    fprintf(fp_txt,"= %.*s =\n", 24, ctime(&curtime));
+    for(int i=0; i<5; i++){
+        if(group>=(i+1)){
+            for(int j=0; j<=6;){
+                do{
+                    j!=6? (arr_num[i][j] = rand()%69+1) : (arr_num[i][j] = rand()%10+1);
+                    flag=0;
+                    for(int k=0; k<j; k++){
+                        temp=0;
+                        if(arr_num[i][j]==arr_num[i][k]){
+                            flag=1;
+                        }
+                        else if(j<5 && arr_num[i][j]<arr_num[i][k]){
+                            temp=arr_num[i][j];
+                            arr_num[i][j]=arr_num[i][k];
+                            arr_num[i][k]=temp;
+                        }
+                    }
+                }while(flag==1);
+                j++;
+            }
+        }
+    }
+
+    for(int i=0; i<5; i++){
+        fprintf(fp_txt,"[%d] : ",i+1);
+        for(int j=0; j<=6; j++){
+            if(group>=(i+1)){
+                fprintf(fp_txt,"%02d ",arr_num[i][j]);
+            }
+            else{
+                fprintf(fp_txt,"-- ");
+            }
+        }
+        fprintf(fp_txt,"\n");
+    }
+    fprintf(fp_txt,"========* Op.0000%d *=========\n",ID);
+    fprintf(fp_txt,"========= csie@CGU =========\n");
+    printf("已為你購買的%d組樂透組合輸出至%s\n",group,filename);
+    fclose(fp_txt);
+
+    count[0]++;
+    fp_bin=fopen("count.bin","wb+");
+    fwrite(count,sizeof(count),1,fp_bin);
+    fclose(fp_bin);
+}
