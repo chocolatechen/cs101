3dc0a3a1a5082c787c5bd6856fdd253d3990dab5
chocolatechen <100300494+chocolatechen@users.noreply.github.com>
2022-06-23 16:54:19 +0800

    fix qz6

--- qz6/main.cpp
+++ qz6/main.cpp
@@ -1,93 +1,49 @@
 #include<iostream>
 #include<string>
-
 using namespace std;
 
 class Clock {
-	public:
-		int hr, min, sec;
-		string mer;
-		Clock(int a, int b, int c, string d) {
-			hr = a;
-			min = b;
-			sec = c;
-			mer = d;
-		}
-		display() {
-			if (hr < 1 || hr > 12) {
-				cout << "hour is out of range" << endl;
-			} else  if (min < 0 || min > 59) {
-				cout << "minute is out of range" << endl;
-			} else if (sec < 0 || sec > 59) {
-				cout << "second is out of range" << endl;
-			} else if (mer != "AM" || mer != "PM") {
-				cout << "meridian is wrong" << endl;
-			} else {
-				cout << hr << ":" << min << ":" << sec << " " << mer << endl;
-			}
-		}
+    public:
+    int hour,minute,second;
+    string meridian;
+    Clock(int H,int M,int S, s tring mer) {
+        hour = H;
+        minute = M;
+        second = S;
+        meridian = mer;
+    }
+    display() {
+        cout << hour << ":" << minute << ":" << second << " " << meridian << endl;
+    }
 };
 
 class StandardClock : Clock {
-	public:
-		StandardClock(int a, int b, int c, string d) : Clock(a, b, c, d){
-			hr = a;
-			min = b;
-			sec = c;
-			mer = d;
-		}
-		display() {
-			if (hr < 1 || hr > 12) {
-				cout << "hour is out of range" << endl;
-			} else  if (min < 0 || min > 59) {
-				cout << "minute is out of range" << endl;
-			} else if (sec < 0 || sec > 59) {
-				cout << "second is out of range" << endl;
-			} else if (mer != "AM" || mer != "PM") {
-				cout << "meridian is wrong" << endl;
-			} else {
-				if (mer == "AM") {
-					cout << "上午 " << hr << ":" << min << ":" << sec << endl;
-				} else if (mer == "PM") {
-					cout << "下午 " << hr << ":" << min << ":" << sec << endl;
-				}
-			}
-		}
+    public:
+    StandardClock(int H, int M, int S, string mer):Clock(H, M, S,mer) {
+        if(meridian == "AM") meridian = "上午";
+        else if(meridian == "PM") meridian = "下午";
+    }
+    display() {
+        cout << meridian << " " << hour << ":" << minute << ":" << second << endl;
+    }
 };
 
 class MilitaryClock : Clock {
-	public:
-		MilitaryClock(int a, int b, int c, string d) : Clock(a, b, c, d){
-			hr = a;
-			min = b;
-			sec = c;
-			mer = d;
-		}
-		display() {
-			if (hr < 1 || hr > 12) {
-				cout << "hour is out of range" << endl;
-			} else  if (min < 0 || min > 59) {
-				cout << "minute is out of range" << endl;
-			} else if (sec < 0 || sec > 59) {
-				cout << "second is out of range" << endl;
-			} else if (mer != "AM" || mer != "PM") {
-				cout << "meridian is wrong" << endl;
-			} else {
-					if (mer == "AM") {
-						cout << hr << ":" << min << ":" << sec << endl;
-					} else if (mer == "PM") {
-						cout << hr+12 << ":" << min << ":" << sec << endl;
-					}
-			}
-		}
+    public:
+    MilitaryClock(int H, int M, int S, string mer):Clock(H, M, S, mer) {
+        if(meridian=="PM") hour+=12;
+    }
+    display() {
+        cout <<  hour << ":" << minute << ":" << second;
+    }
 };
 
 int main() {
-	Clock cc(10, 25, 43, "AM");
-	StandardClock sc(10, 25, 43, "AM");
-	MilitaryClock mc(10, 25, 43, "PM");
-	cc.display();
-	sc.display();
-	mc.display();
-	return 0;
+    Clock cc(10,25,43,"AM");
+    StandardClock sc(10,25,43,"AM");
+    MilitaryClock mc(10,25,43,"PM");
+    cc.display();
+    sc.display();
+    mc.display();
+    return 0;
 }
